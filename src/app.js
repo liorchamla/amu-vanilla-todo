3a10a7ac46d9634bba12ba392f97a3a821b2ca7b
Lior Chamla <contact@web-develop.me>
2021-11-29 14:42:03 +0100

    Récupération API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
-const TODO_ITEMS = [
-  { id: 1, text: "Faire les courses", done: false },
-  { id: 2, text: "Aller chercher les enfants", done: true },
-];
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("https://ubrnopsjlvwleakngnmr.supabase.co/rest/v1/todos", {
+    headers: {
+      apiKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzODE5MjQ4MSwiZXhwIjoxOTUzNzY4NDgxfQ.3JdQW11rNZNpvcehhwFVaofXL2agE5LDn_3O4BvSAHw",
+    },
+  })
+    .then((response) => response.json())
+    .then((items) => {
+      items.forEach((item) => addTodo(item));
+    });
+});
 
 const addTodo = (item) => {
   const container = document.querySelector("ul");
@@ -20,8 +28,6 @@ const addTodo = (item) => {
     `
   );
 };
-
-TODO_ITEMS.forEach((item) => addTodo(item));
 
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
